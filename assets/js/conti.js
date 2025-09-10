6f0d5e3cc4edb20549a96a95c482e0b87b8711f0
Charles856 <cs524902@gmail.com>
2025-09-10 08:31:43 -0300

    Add files via upload

--- assets/js/conti.js
+++ assets/js/conti.js
@@ -1,30 +1,28 @@
-const button=document.getElementById("messi")
-const cidade=document.getElementById("cidade")
-const estado=document.getElementById("estado")
-const lagadouro=document.getElementById("lagadouro")
-const bairro=document.getElementById("bairro")
-const inputCep=document.getElementById("cep")
-const complemento=document.getElementById("complemento")
+const button = document.getElementById("messi")
+const cidade = document.getElementById("cidade")
+const estado = document.getElementById("estado")
+const lagadouro = document.getElementById("lagadouro")
+const bairro = document.getElementById("bairro")
+const inputCep = document.getElementById("cep")
+const complemento = document.getElementById("complemento")
 
-async function buscarEndereço(cp) {
-    const reponse = await fetch(`https://viacep.com.br/ws/${cp}/json`)
-    const data = await reponse.json()
+async function buscarEndereco(cp) {
+
+    let objetocep = await fetch(`https://viacep.com.br/ws/${cp}/json`);
+    console.log(objetocep)
+    let data = await objetocep.json();
     console.log(data)
 
     cidade.value = data.localidade
     estado.value = data.uf
-    lagadouro.value =data.logradouro
+    lagadouro.value = data.logradouro
     bairro.value = data.bairro
     complemento.value = data.complemento
-
-
 }
 
 
-
-
-addEventListener('click', function(){
-    const cep = inputCep.value
-    buscarEndereço(cep)
-})
-
+inputCep.addEventListener("change", function (evento) {
+    evento.preventDefault()
+    buscarEndereco(inputCep.value)
+}
+)
